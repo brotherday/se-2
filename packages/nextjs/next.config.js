12983e32e783f605cde236b917d6bf1ace07abaf
Steve Huguenin-Elie <steve.huguenin-elie@protonmail.ch>
2023-03-19 12:03:10 +0100

    Minimal distribution with chakra-ui and inferno

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -2,6 +2,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, {dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.resolve.alias = {
+        react: "inferno-compat",
+        "react-dom": "inferno-compat",
+        ...config.resolve.alias
+      };
+    }
+    return config;
+  },
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
